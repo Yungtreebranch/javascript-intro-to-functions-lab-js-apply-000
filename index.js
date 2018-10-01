045378af5e583aeab45b915a54fe561ea6252bfa
Gregory Williams <hooper4lif13@hotmail.com>
2018-10-01 03:12:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 function  shout(String) {
   return String.toUpperCase() 
-}
+} 
 function whisper(string) {
   return String.toLowerCase()
-}
+} 
 function logShout(String) {
   console.log(string.toUpperCase)
 }
