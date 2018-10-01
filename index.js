a16e7275de13ecba5f3dad19f6f2417424b440e1
Gregory Williams <hooper4lif13@hotmail.com>
2018-10-01 03:29:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,4 @@ function logWhisper(string) {
   console.log (string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (String.toLowerCase() === ) {
-    return "I can't hear you!"
-  } else if (string.toUpperCase() === ) {
-    return "Yes Indeed!" ;
-  } else if (string === "I Love you, Grandma.") {
-    return "I love you, too.";
-  }
+  
