149f9f82595f80be1d20b6096ceb80a830524447
Gregory Williams <hooper4lif13@hotmail.com>
2018-10-01 03:24:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === ) {
     return "I can't hear you!"
   } else if (string.toUpperCase() === ) {
-    return "Yes Indeed"
+    return "Yes Indeed!"
   }
 }
